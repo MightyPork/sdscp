dbc46b4d0663fc4619106420146daeb2db06b8ef
Ondřej Hruška <ondra@ondrovo.com>
2014-08-04 08:35:48 +0200

    Some new demo C file

---
+++ test/aaa.c
@@ -0,0 +1,46 @@
+// Makro v makru
+#define RELE1 sys[231]
+#define SVETLO RELE1
+// teď funguje!
+
+
+// Malá pásmena v názvu makra!
+#define verze "4.5.6" // <-- Dvojté uvozovky
+
+
+// Funkční makro
+#define Secti(a, b) ((a) + (b))
+
+
+// Makro s hranatými závorkami (array-like)
+#define SQUARES[i] ((i) * (i))
+// např. SQUARES[4]
+
+// Zatím nefunguje (omezení SDS-C), ale brzo bude!
+#define RELE[i] sys[230 + (i)]
+
+
+// Podmíněné větvení překladu
+
+#define DEBUG
+
+#ifdef DEBUG
+	echo('Dlouha debug zprava');
+	echo('Dalsi debuga');
+#else
+	echo('Strucna zprava');
+#endif
+
+
+// SDSCP pochopitelně umí #include :)
+
+#include "konstanty.c"
+#include "utility.c"
+
+
+// === Tohle je v plánu umožnit ===
+funkce(s, argumenty)
+{
+	// ...
+	return 123;
+}
