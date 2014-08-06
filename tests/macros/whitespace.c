551a42109fcf7e8346ffdfe76a5277f809563efa
Ondřej Hruška <ondra@ondrovo.com>
2014-08-06 11:27:56 +0200

    changes in whitespace.c

--- tests/macros/whitespace.c
+++ tests/macros/whitespace.c
@@ -3,11 +3,17 @@
 										http_get(ip1, ip2, ip3, ip4, "localhost",		\
 											route "?id=", ID, ## args					\  // "&aaa=", aaa, "&bbb=", bbb ...
 										);												\
+
 										wait_for_http_get();							\
+
 										if ( HTTP_GET_OK() ) {							\
+
 											echo("GET success: ", HTTP_GET_RESULT());	\
+
 										} else {										\
+
 											echo("GET error: ", HTTP_GET_RESULT() );	\
+
 										}
 
 
